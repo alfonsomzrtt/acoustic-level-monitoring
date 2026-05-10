2ecb0b3564a0561c774e7ee5fb362841cef2f2e6
alfonsomzrtt <alfonsomzrt@gmail.com>
2026-05-10 19:19:40 +0700

    Adding toggle theme switch

--- web-app/frontend/js/overview.js
+++ web-app/frontend/js/overview.js
@@ -239,3 +239,26 @@ client.connect({
 //   reconnect: true
 });
 
+const toggleSwitch = document.querySelector('#theme-toggle');
+const currentTheme = localStorage.getItem('theme');
+
+// Cek apakah sebelumnya sudah memilih Dark Mode
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+  if (currentTheme === 'dark') {
+      toggleSwitch.checked = true;
+  }
+}
+
+// Fungsi untuk mengganti tema
+function switchTheme(e) {
+  if (e.target.checked) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      localStorage.setItem('theme', 'dark');
+  } else {
+      document.documentElement.setAttribute('data-theme', 'light');
+      localStorage.setItem('theme', 'light');
+  }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
