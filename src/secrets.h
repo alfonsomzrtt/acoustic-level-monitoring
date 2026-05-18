6f34de72e7717c33086051dbe2853823a8cf03dd
Alfonso Mayzart <alfonsomzrt@gmail.com>
2026-05-18 11:37:32 +0700

    added secrets.h file WiFi and MQTT config

---
+++ src/secrets.h
@@ -0,0 +1,11 @@
+// secrets.h
+
+// WiFi Configuration
+#define SECRET_SSID "Isi SSD WiFi di sini"
+#define SECRET_WIFI_PASS "Isi password WiFi di sini"
+
+// MQTT Configuration //Ini kredensial dari ESP32 yang memiliki akses PUBLISH-ONLY
+#define SECRET_MQTT_SERVER "isi url mqtt hivemq cloud"
+#define SECRET_MQTT_PORT //isi dengan port MQTTnya, semisal 8888
+#define SECRET_MQTT_USER "isi dengan username mqtt" // Username yang punya hak Publish
+#define SECRET_MQTT_PASS "Isi password kredensial"
